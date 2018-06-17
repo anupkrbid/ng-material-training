6da42a965a2196c1a664799b1cbebf8a2d6da02a
anupkrbid <anupbid94@gmail.com>
2018-06-17 20:15:27 +0530

    Setup Navigations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { MaterialModule } from './shared/material.module';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule,
     MaterialModule
   ],
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -5,6 +5,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatNativeDateModule } from '@angular/material';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -20,6 +21,7 @@ import { NgModule } from '@angular/core';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     MatNativeDateModule,
     MatSidenavModule,
     MatToolbarModule
@@ -32,6 +34,7 @@ import { NgModule } from '@angular/core';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     MatNativeDateModule,
     MatSidenavModule,
     MatToolbarModule
