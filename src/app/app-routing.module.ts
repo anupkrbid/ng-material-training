bbb98aa1bc51c5d599b5e0625eee1171f6265d46
anupkrbid <anupbid94@gmail.com>
2018-06-20 06:51:26 +0530

    Implemented Route Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './auth/auth.guard';
+import { NotAuthGuard } from './auth/not-auth.guard';
+
 const routes: Routes = [
-  { path: '', loadChildren: './home/home.module#HomeModule' },
-  { path: 'sign-in', loadChildren: './auth/sign-in/sign-in.module#SignInModule' },
-  { path: 'sign-up', loadChildren: './auth/sign-up/sign-up.module#SignUpModule' },
-  { path: 'training', loadChildren: './training/training.module#TrainingModule' },
+  { path: '', canActivate: [NotAuthGuard], loadChildren: './home/home.module#HomeModule' },
+  { path: 'sign-in', canActivate: [NotAuthGuard], loadChildren: './auth/sign-in/sign-in.module#SignInModule' },
+  { path: 'sign-up', canActivate: [NotAuthGuard], loadChildren: './auth/sign-up/sign-up.module#SignUpModule' },
+  { path: 'training', canActivate: [AuthGuard], loadChildren: './training/training.module#TrainingModule' },
   { path: '**', redirectTo: 'sign-in' }
 ];
 
